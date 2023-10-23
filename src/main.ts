cbc961e8cd30fd00e9b9f4c4cae831d0a5bae25b
Jeevithan Mahenthran <JeevithanMahenthran@gmail.com>
2023-10-22 22:59:43 -0700

    step 1

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
+const canvas: HTMLCanvasElement = document.createElement("canvas");
 
-const gameName = "My game";
+const gameName = "Jeevi's Game";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+canvas.width = 256;
+canvas.height = 256;
+canvas.style.border = "2px solid black";
+canvas.style.borderRadius = "10px";
+canvas.style.boxShadow = "3px 3px 5px gray";
+canvas.style.backgroundColor = "white";
+app.appendChild(canvas);
