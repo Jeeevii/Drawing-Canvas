7d3bad28cd06256dbab199cf02a1a59a33b7fb83
Jeevithan Mahenthran <JeevithanMahenthran@gmail.com>
2023-10-22 23:08:01 -0700

    step 2 finished, able to draw and clear

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 const canvas: HTMLCanvasElement = document.createElement("canvas");
+const context2D: CanvasRenderingContext2D | null = canvas.getContext("2d");
 
 const gameName = "Jeevi's Game";
 
@@ -17,4 +18,44 @@ canvas.style.border = "2px solid black";
 canvas.style.borderRadius = "10px";
 canvas.style.boxShadow = "3px 3px 5px gray";
 canvas.style.backgroundColor = "white";
-app.appendChild(canvas);
+app.append(canvas);
+
+let drawing = false;
+
+if (context2D) {
+  canvas.addEventListener("mousedown", (e) => {
+    drawing = true;
+    context2D.beginPath();
+    context2D.moveTo(
+      e.clientX - canvas.getBoundingClientRect().left,
+      e.clientY - canvas.getBoundingClientRect().top
+    );
+  });
+
+  canvas.addEventListener("mousemove", (e) => {
+    if (!drawing) return;
+    context2D.lineTo(
+      e.clientX - canvas.getBoundingClientRect().left,
+      e.clientY - canvas.getBoundingClientRect().top
+    );
+    context2D.stroke();
+  });
+
+  canvas.addEventListener("mouseup", () => {
+    drawing = false;
+  });
+
+  canvas.addEventListener("mouseout", () => {
+    drawing = false;
+  });
+}
+
+const clearButton = document.createElement("button");
+clearButton.textContent = "clear";
+app.append(clearButton);
+
+clearButton.addEventListener("click", () => {
+  if (context2D) {
+    context2D.clearRect(0, 0, canvas.width, canvas.height);
+  }
+});
